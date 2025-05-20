718ac4907f8aced904fcaaa33e91f1ef489fc065
juliameller <juliameller@live.com>
2025-05-20 20:11:40 -0300

    .

--- App.js
+++ App.js
@@ -1,12 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+
+
+// function HomeScreen() {
+// const navigation = useNavigation();
+
+//   <View style={styles.container}>
+//   <Text style={{fontSize: 36}}>Home Screen</Text>
+//   <Button title='IR PARA SECOND'
+//   onPress={() => {
+//     navigation.navigate("Second")
+//   }}></Button>
+// </View>
+// }
+
+function MoviesScreen() {
+  const navigation = useNavigation();
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  <View>
+  <Button title="Listar filmes"
+  onPress={() => navigation.navigate("Movies")}
+  ></Button>
+</View>
+
+
+const getMovies = async () => {
+  try {
+  const response = await fetch("https://reactnative.dev/movies.json");
+  const json = await response.json();
+  setData(json.movies);
+  } catch (error) {
+  console.error(error);
+  } finally {
+  setLoading(false);
+  }
+  }
+
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    {isLoading ? (
+    <ActivityIndicator />
+    ) : (
+    <View>
+    {data.map((item) => (
+    <Text key={item.id}>{item.title} - {item.releaseYear}</Text>
+    ))}
+    </View>
+    )}
+    </View>
+}
+
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Movies" component={MoviesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
